f52eafe24b1d4e627aa20e27047044ce56c9c30a
Darshan Patel <143177575+DarshanPatel311@users.noreply.github.com>
2023-12-21 19:57:54 +0530

    Add files via upload

---
+++ day2.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Declare and initialize variables of different data types
+  int age = 25;
+  double height = 5.9;
+  bool isStudent = true;
+  String name = "John Doe";
+
+  // Create a list
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Create a set
+  Set<String> uniqueNames = {"John", "Jane", "Bob"};
+
+  // Create a map
+  Map<String, dynamic> person = {
+    "name": "Alice",
+    "age": 30,
+    "isStudent": false,
+  };
+
+  // Print the values
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student? $isStudent");
+  print("Name: $name");
+  print("Numbers: $numbers");
+  print("Unique Names: $uniqueNames");
+  print("Person: $person");
+}
