fcca2fe64c3c1ca4b46dc32776ee70c9a713644b
Darshan Patel <143177575+DarshanPatel311@users.noreply.github.com>
2024-01-10 19:04:53 +0530

    Add files via upload

---
+++ Lecture 3/pr1.dart
@@ -0,0 +1,12 @@
+void main() {
+  Map m1 = {
+    "id": 3572,
+    "name": "darshan patel",
+    "Age": 18,
+    "salary": 1000000,
+  };
+  print(m1['id']);
+  print(m1['name']);
+  print(m1['Age']);
+  print(m1['salary']);
+}
