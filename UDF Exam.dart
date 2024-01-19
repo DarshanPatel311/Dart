3a2257a573ab2fe0a2e6155837a25e487f332935
Darshan Patel <143177575+DarshanPatel311@users.noreply.github.com>
2024-01-19 18:06:58 +0530

    Add files via upload

---
+++ UDF Exam.dart
@@ -0,0 +1,29 @@
+void main() {
+  print(rockey());
+  print("~~~~Shoot Garuda Rocky~~~~");
+}
+
+int pass1() {
+  int gun1 = 30;
+  return gun1;
+}
+
+int pass2() {
+  int gun2 = 30;
+  return gun2;
+}
+
+int pass3() {
+  int gun3 = 30;
+  return gun3;
+}
+
+int delar(int pass1, int pass2, int pass3) {
+  int delar = pass1 + pass2 + pass3;
+  return delar;
+}
+
+int rockey() {
+  int rockey = 10 + delar(pass1(), pass2(), pass3());
+  return rockey;
+}
